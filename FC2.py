d3a0db22d691124f1d450c43c05fdf160c1cfa72
AirbleDellen <145096853+AirbleDellen@users.noreply.github.com>
2023-09-17 22:02:19 +0800

    Create FC2.py

---
+++ FC2.py
@@ -0,0 +1,23 @@
+import time
+import os
+
+def countdown_timer(minutes):
+    seconds = minutes * 60
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        timer_display = f"{mins:02d}:{secs:02d}"
+        print(f"Time left: {timer_display}", end='\r')
+        time.sleep(1)
+        seconds -= 1
+
+    print("\nTime's up! Stay focused!")
+
+def main():
+    try:
+        minutes = int(input("请输入专注的分钟数 单位：min: "))
+        countdown_timer(minutes)
+    except ValueError:
+        print("无效输入，请输入一个整数。")
+
+if __name__ == "__main__":
+    main()
