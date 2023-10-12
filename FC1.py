c8da49b36816a66c58138df8efe5fde4b3bb2bc0
AirbleDellen <145096853+AirbleDellen@users.noreply.github.com>
2023-10-12 22:33:34 +0800

    Update FC1.py

--- FC1.py
+++ FC1.py
@@ -14,7 +14,7 @@ def countdown_timer(minutes):
 
 def main():
     try:
-        minutes = int(input("请输入专注的分钟数: "))
+        minutes = int(input("请输入您愿意专注的分钟数: "))
         countdown_timer(minutes)
     except ValueError:
         print("无效输入，请输入一个整数。")
